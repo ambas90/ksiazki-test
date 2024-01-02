52b545396b14258f5cbd87dacf3809bfc93d38ba
Dawid Jasiński <ambas90@gmail.com>
2024-01-02 20:15:21 +0100

    Update book-list.mjs

--- js/book-list.mjs
+++ js/book-list.mjs
@@ -1,35 +1,39 @@
 //deklaracja zmiennych
-const bookListHeader = document.querySelector('.bl-header');
-const bookList = document.querySelector('.bl-container');
+const bookListHeader = document.querySelector(".bl-header");
+const bookList = document.querySelector(".bl-container");
 
 bookListHeader.innerHTML = 'Best Sellers <span class="bl-span">Books</span>';
 
 //funkcja pobierająca liste książek
-async function booksFetch(category = 'top-books') {
-  if (category === 'All Categories') {
-    category = 'top-books';
+async function booksFetch(category = "top-books") {
+  let categoryId = category;
+  if (category === "All Categories") {
+    categoryId = "top-books";
   }
-  const baseUrl = 'https://books-backend.p.goit.global/books/';
-  if (category != 'top-books') {
-    category = `category?category=${category}`;
+  const baseUrl = "https://books-backend.p.goit.global/books/";
+  if (category !== "top-books") {
+    categoryId = `category?category=${category}`;
   }
-  const response = await fetch(baseUrl + category);
-  const books = await response.json();
-  if (category === 'top-books') {
-    allBooksRender(books);
-  } else {
-    categoryBooksRender(books);
+  try {
+    const response = await fetch(baseUrl + categoryId);
+    const books = await response.json();
+    if (category === "top-books") {
+      allBooksRender(books);
+    } else {
+      categoryBooksRender(books);
+    }
+  } catch (error) {
+    console.error("Error fetching books:", error);
   }
 }
-
 booksFetch();
 
 //funkcja wyświetlająca wszystkie książki
 function allBooksRender(books) {
-  let bookListHtml = '';
-  books.forEach(bookCategory => {
+  let bookListHtml = "";
+  books.forEach((bookCategory) => {
     bookListHtml += `<p class="bl-book-category">${bookCategory.list_name}</p><ul class="bl-books-list">`;
-    bookCategory.books.forEach(book => {
+    bookCategory.books.forEach((book) => {
       bookListHtml += `<li class="bl-book-card bl-all-books" id="${book._id}">
       <div class="bl-book-image">
       <img src="${book.book_image}" alt="${book.title}"/>
@@ -44,16 +48,16 @@ function allBooksRender(books) {
     bookListHtml += `</ul><button id="${bookCategory.list_name}"class="bl-see-more-btn">SEE MORE</button>`;
   });
   bookList.innerHTML = bookListHtml;
-  const seeMoreBtn = document.querySelectorAll('.bl-see-more-btn');
-  seeMoreBtn.forEach(btn => {
-    btn.addEventListener('click', btnClickHandler);
+  const seeMoreBtn = document.querySelectorAll(".bl-see-more-btn");
+  seeMoreBtn.forEach((btn) => {
+    btn.addEventListener("click", btnClickHandler);
   });
 }
 
 //funkcja wyświetlająca wszystkie książki z danej kategorii
 function categoryBooksRender(books) {
   let bookListHtml = '<ul class="bl-books-list">';
-  books.forEach(book => {
+  books.forEach((book) => {
     bookListHtml += `<li class="bl-book-card" id="${book._id}">
       <div class="bl-book-image">
       <img src="${book.book_image}" alt="${book.title}"/>
@@ -65,7 +69,7 @@ function categoryBooksRender(books) {
       <p class="bl-book-author">${book.author}</p>
       </li>`;
   });
-  bookListHtml += '</ul>';
+  bookListHtml += "</ul>";
   bookList.innerHTML = bookListHtml;
 }
 
@@ -77,18 +81,18 @@ function btnClickHandler() {
 }
 
 //funkcja przekazująca kategorię ksiązek z menu do funkcji pobierającej listę książek
-placeForList.addEventListener('click', ev => {
+placeForList.addEventListener("click", (ev) => {
   headerRender(ev.target.textContent);
   booksFetch(ev.target.textContent);
 });
 
 //funkcja renderująca tytuł kategorii
 function headerRender(categoryName) {
-  if (categoryName === 'All Categories') {
-    categoryName = 'Best Sellers Books';
+  if (categoryName === "All Categories") {
+    categoryName = "Best Sellers Books";
   }
-  const headerArr = categoryName.split(' ');
+  const headerArr = categoryName.split(" ");
   const lastWord = headerArr.pop();
-  const header = headerArr.join(' ');
+  const header = headerArr.join(" ");
   bookListHeader.innerHTML = `${header} <span class="bl-span">${lastWord} </span>`;
 }
